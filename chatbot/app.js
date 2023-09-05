7246f91ac28fb93cc604cb3384cc301b32cc15e2
Setthasit Poosawat <setthasit.developer@gmail.com>
2023-09-06 01:51:56 +0700

    commit

--- chatbot/app.js
+++ chatbot/app.js
@@ -34,7 +34,7 @@ app.post("/webhook", async (req, res) => {
   //   console.error(error);
   //   res.status(500).send("Error in webhook request");
   // }
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 3000, () => {
